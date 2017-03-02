9e5c1eb0941ce0184289b5b55063165be5c2df48
Fries <ak532892@gmail.com>
2017-03-02 18:13:50 +0800

    Add files via upload

--- UVa/AOAPC II Beginning Algorithm Contests (Second Edition) (Rujia Liu)/Chapter 3. Arrays and Strings/Exercises/1587 - Box.cpp
+++ UVa/AOAPC II Beginning Algorithm Contests (Second Edition) (Rujia Liu)/Chapter 3. Arrays and Strings/Exercises/1587 - Box.cpp
@@ -10,38 +10,23 @@ int cmp(struct pallet a, struct pallet b){
 	return a.w < b.w;
 }
 int main(){
-	int w, h;
+	struct pallet p[6];
 
-	while(cin>>w>>h){
-		struct pallet p[6];
-		int seq[12];
-
-		if(w > h)	swap(w, h);
-		seq[0] = p[0].w = w, seq[1] = p[0].h = h;
+	while(cin>>p[0].w>>p[0].h){
+		if(p[0].w > p[0].h)	swap(p[0].w, p[0].h);
 
 		for(int i = 1; i < 6; i++){
-			cin>>w>>h;
-			if(w > h)	swap(w, h);
-			seq[2*i] = p[i].w = w, seq[2*i + 1] = p[i].h = h;
-		}
-
-		sort(seq, seq + 12);
-		
-		int imp = 0, dup[3];
-		for(int i = 0; i < 3; i++){
-			for(int j = 1; j < 4; j++){
-				if(seq[i*4] != seq[i*4 + j])
-					imp = 1;
-			}
-			dup[i] = seq[i*4];
-		}
-
-		if(imp){
-			cout<<"IMPOSSIBLE\n";
-			continue;
+			cin>>p[i].w>>p[i].h;
+			if(p[i].w > p[i].h)	
+				swap(p[i].w, p[i].h);
 		}
 
 		sort(p, p + 6, cmp);
+		int dup[3];
+
+		dup[0] = p[0].w;
+		dup[1] = p[0].h;
+		dup[2] = p[2].h;
 
 		if( p[0].w != dup[0] || p[0].h != dup[1] ||
 			p[1].w != dup[0] || p[1].h != dup[1] || 
